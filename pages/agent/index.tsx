6371cfb969f5e682a14c66a2eab768b61cf3977c
fayzullaevfarrukh98 <fayzullaevfarrukh98@gmail.com>
2026-05-10 15:50:55 +0900

    feat: develop mobile version

--- pages/agent/index.tsx
+++ pages/agent/index.tsx
@@ -1,15 +1,20 @@
-
 import withLayoutBasic from "@/libs/components/layout/LayoutBasic";
-import { Container, Stack } from "@mui/material";
+import useDeviceDetect from "@/libs/hooks/useDeviceDetect";
+import { Stack } from "@mui/material";
 import { NextPage } from "next";
 
 const AgentList: NextPage = () => {
-  return (
-    <div style={{ margin: "20px 0" }}>
-      {" "}
-      <Stack className='container'>AGENT LIST</Stack>
-    </div>
-  );
+  const device = useDeviceDetect();
+
+  if (device === "mobile") {
+    return <Stack>AGENTS MOBILE</Stack>;
+  } else {
+    return (
+      <div style={{ margin: "20px 0" }}>
+        <Stack className="container">AGENTS LIST</Stack>
+      </div>
+    );
+  }
 };
 
 export default withLayoutBasic(AgentList);
--- pages/index.tsx
+++ pages/index.tsx
@@ -1,21 +1,28 @@
-import { Stack } from "@mui/material";
-
-import { NextPage } from "next";
+import Advertisement from "@/libs/components/homepage/Advertisement";
+import PopularProperties from "@/libs/components/homepage/PopularProperties";
+import TopAgents from "@/libs/components/homepage/TopAgents";
+import TopProperties from "@/libs/components/homepage/TopProperties";
 import TrendProperties from "../libs/components/homepage/TrendProperty";
 import withLayoutMain from "@/libs/components/layout/LayoutHome";
-import PopularProperties from "@/libs/components/homepage/PopularProperties";
-import Advertisement from "@/libs/components/homepage/Advertisement";
-import TopProperties from "@/libs/components/homepage/TopProperties";
-import TopAgents from "@/libs/components/homepage/TopAgents";
+import useDeviceDetect from "@/libs/hooks/useDeviceDetect";
+import { Stack } from "@mui/material";
+import { NextPage } from "next";
+
 const Home: NextPage = () => {
-  return (
-    <Stack className={"home-page"}>
-      <TrendProperties />
-      <PopularProperties />
-      <Advertisement />
-      <TopProperties />
-      <TopAgents />
-    </Stack>
-  );
+  const device = useDeviceDetect();
+
+  if (device === "mobile") {
+    return <Stack>HOPMEPAGE MOBILE</Stack>;
+  } else {
+    return (
+      <Stack className={"home-page"}>
+        <TrendProperties />
+        <PopularProperties />
+        <Advertisement />
+        <TopProperties />
+        <TopAgents />
+      </Stack>
+    );
+  }
 };
 export default withLayoutMain(Home);
